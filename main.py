d8d06bfefafa58c2cb3f20d594c5701d05888477
Anthony <tosharus26@yandex.ru>
2022-10-04 14:00:59 +0400

    first

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ import warnings
 
 sns.set(rc={'figure.figsize': (12, 7)})
 
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
+}
+
 
 def get_data_from_hh(url):
-    data = requests.get(url, timeout=5).json()
+    data = requests.get(url, headers=headers, timeout=5).json()
     return data
 
 
