dabbb55f48432a3004bc8ea81b04dcdea5b67949
Anthony <tosharus26@yandex.ru>
2022-10-04 14:01:19 +0400

    прикрутил прокси

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import warnings
 
 sns.set(rc={'figure.figsize': (12, 7)})
 
+
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
